0747abb3fb2777b54c28a7b8e5fdb20410abf1cc
Alefe Silva <alefeans2@hotmail.com>
2017-11-06 17:47:45 -0200

    I'll say nothing... but this is the last time i'mma do this.

---
+++ hacker_rank/30_days_challenge/day_11/2d_arrays.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3.6
+import sys
+
+arr = []
+for arr_i in range(6):
+   arr_t = [int(arr_temp) for arr_temp in input().strip().split(' ')]
+   arr.append(arr_t)
+
+res = []
+for x in range(0, 4):
+    for y in range(0, 4):
+        s = sum(arr[x][y:y+3]) + arr[x+1][y+1] + sum(arr[x+2][y:y+3])
+        res.append(s)
+
+print(max(res))
