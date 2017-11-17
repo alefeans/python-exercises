84aaa49f7de5d378b6d7d8c0a85b0790fb3e4d93
Alefe Silva <alefeans2@hotmail.com>
2017-11-17 10:32:00 -0200

    Bubble sort in python is very idiot hahaha. Built-in functions for the win (sort()).

--- hacker_rank/30_days_challenge/day_20/sorting.py
+++ hacker_rank/30_days_challenge/day_20/sorting.py
@@ -1,7 +1,31 @@
 #!/usr/bin/python3
 import sys
 
+# Oh sort().. how i miss u
+
+def validator(n, a):
+    if (n < 2) and (n > 600):
+        sys.exit(1)
+    for i in range(len(a)):
+        if (a[i] < 1) and (a[i] < 2 * 10**6):
+            sys.exit(1)
+
+def bubble_sort(a):
+    swaps = 0
+    for i in range(len(a)):
+        for j in range(len(a) - 1):
+            if (a[j] > a[j + 1]):
+                a[j], a[j + 1] = a[j + 1], a[j]
+                swaps += 1
+        if (swaps == 0):
+            break
+    return swaps, a
+
+
 n = int(input().strip())
 a = list(map(int, input().strip().split(' ')))
-# Write Your Code Here
-print("test")
+validator(n, a)
+swaps, a = bubble_sort(a)
+print("Array is sorted in {} swaps.".format(swaps))
+print("First Element: {}".format(a[0]))
+print("Last Element: {}".format(a[n - 1]))
---
+++ hacker_rank/30_days_challenge/day_21/another_day.py
@@ -0,0 +1,1 @@
+print("Another day without python support......... :()")
